49fac3af62693cf3cda40da52c09f37845df04e5
Yijin <leowyijin@gmail.com>
2016-01-31 17:19:12 +0800

    added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var babel = require("gulp-babel");
+const gulp = require('gulp');
+const mocha = require('gulp-mocha');
+const babel = require("gulp-babel");
+const help = require("gulp-task-listing");
+const eslint = require("gulp-eslint");
 
-var TESTS = 'test/*.js';
-var REPORTER = 'dot';
+const TESTS = 'test/*.js';
+const REPORTER = 'dot';
 
-gulp.task("default", ["babel"]);
+gulp.task("help", help);
+
+gulp.task("default", ["test", "lint", "builds"]);
+
+gulp.task("lint", function () {
+    return gulp.src(["**/*.js", "!node_modules/**"])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
 
 gulp.task('test', function(){
     return gulp.src(TESTS, {read: false})
@@ -13,7 +24,10 @@ gulp.task('test', function(){
       timeout: 2000,
       reporter: REPORTER,
       bail: true,
-      globals: ['___eio', 'document']
+      globals: [
+          '___eio',
+          'document'
+      ]
     }))
     .once('error', function(){
       process.exit(1);
